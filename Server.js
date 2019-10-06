cac71560152c972e9b5f71ff6b9eb37855865208
Matthew Mong <matthew.mong1999@gmail.com>
2019-10-06 16:13:02 -0700

    Add POST

--- Server.js
+++ Server.js
@@ -4,10 +4,12 @@ const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: 
 var express = require('express');
 //var bodyParser = require('body-parser');
 var app = express();
+app.use(express.json());
 var db;
 var ObjectID = require('mongodb').ObjectID;
 const port = 3000;
-app.use(express.json());
+
+
 // var server = app.listen(8081, function(){
 //     var host = server.address().address;
 //     var port = server.address().port;
@@ -28,12 +30,51 @@ client.connect(err => {
 //     console.log(db.collection("Users").findOne({_id: id}));
 //     return res.json(db.collection("Users").findOne({_id: id}));
 // });
+// app.post('/', (req,res)=>{
+//     db.collection("Users").insertOne({"task":req.body.task,"info":req.body.info}, (err, result)=>{
+//         if (err) return console.log(err);
+//         res.send("saved");
+//     })
+// });
+
+
+
+app.post('/Users', function(req,res){
+    db.collection("Users").insertOne(req.body, (err, result)=>{
+        if (err) return console.log(err);
+        res.send(result.insertedId);
+        });
+    });
+
+
+app.post('/Events', function(req,res){
+    db.collection("Users").insertOne(req.body, (err, result)=>{
+        if (err) return console.log(err);
+        res.send(result.insertedId);
+    });
+});
+
+
 app.get('/', (req, res) => {
     db.collection("Users").find().toArray((err,result) => {
         res.send(result);})
 });
 
+//
+// app.post('/Users', (req,res) =>{
+//     console.log("triggered");
+//     console.log(req.body);
+//     // var doc = Document.parse(req.data.toString());
+//     // res.send(doc);
+//     db.collection("Users").insert(req.body,function(err,result) {
+//         if (err)
+//             res.send(err);
+//         else
+//             res.send(result);})
+// });
+
 app.get('/Users/:id', (req, res) => {
+    console.log("someone retrieved a user");
     var id = new ObjectID(req.params.id);//req.params.id
     db.collection("Users").find({'_id':id}).toArray((err,result) => {
         res.send(result);})
