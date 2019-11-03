9faa23fd4fc62855c0760e1db7b2b560bf55d230
Matthew Mong <matthew.mong1999@gmail.com>
2019-11-02 19:17:56 -0700

    ESlint cleanup

--- Server.js
+++ Server.js
@@ -6,34 +6,33 @@ const coord_var = 2.00001;
  * @type {admin}
  */
 
-var admin = require("firebase-admin");
-var serviceAccount = require("../Backend321/thissucks-b5ac7-firebase-adminsdk-389of-ad03ab0675");
+const admin = require('firebase-admin');
+const serviceAccount = require('../Backend321/thissucks-b5ac7-firebase-adminsdk-389of-ad03ab0675');
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://thissucks-b5ac7.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://thissucks-b5ac7.firebaseio.com',
 });
 
-//Init of Mongo Backend for database
+// Init of Mongo Backend for database
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://kswic:rqerBR73CjIcOnaB@test-cluster-323xs.azure.mongodb.net/test?retryWrites=true&w=majority";
+const uri = 'mongodb+srv://kswic:rqerBR73CjIcOnaB@test-cluster-323xs.azure.mongodb.net/test?retryWrites=true&w=majority';
 const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 app.use(express.json());
-var db;
-var ObjectID = require('mongodb').ObjectID;
+let db;
+const ObjectID = require('mongodb').ObjectID;
 
 
 /**
  * Connect to MongoDB server
  */
-client.connect(err => {
-    if (err) return console.log(err);
-    db = client.db('Data');
-    console.log("successful connect");
+client.connect((err) => {
+  if (err) return console.log(err);
+  db = client.db('Data');
+  console.log('successful connect');
 });
-
 
 
 /*
@@ -47,60 +46,60 @@ Parameter: user_id, user_longdec, user_latdec
  * @param res
  * @param next
  */
-var get_user_location = function (req, res, next) {
-    var longitdec = req.body.user_longdec;
-    var latitdec = req.body.user_latdec;
-    var user_id = req.body.user_id;
-    try {
-        db.collection("Users").updateOne(
-            {_id: {user_id}},
-            {
-                $set:
+const get_user_location = function(req, res, next) {
+  const longitdec = req.body.user_longdec;
+  const latitdec = req.body.user_latdec;
+  const user_id = req.body.user_id;
+  try {
+    db.collection('Users').updateOne(
+        {_id: {user_id}},
+        {
+          $set:
                     {
-                        longdec: longitdec,
-                        latdec: latitdec
-                    }
-            }
-        );
-    } catch (e) {
-        console.log(e);
-    }
-    next();
+                      longdec: longitdec,
+                      latdec: latitdec,
+                    },
+        },
+    );
+  } catch (e) {
+    console.log(e);
+  }
+  next();
 };
 app.use(get_user_location);
 
 /**
  *
  */
-app.post('/Users', function (req, res) {
-    db.collection("Users").insertOne(req.body, (err, result) => {
+app.post('/Users', function(req, res) {
+  db.collection('Users').insertOne(req.body, (err, result) => {
+    if (err) return console.log(err);
+    res.send(result.insertedId);
+  });
+});
+
+/**
+ *
+ */
+app.put('/Users/:id', function(req, res) {
+  const id = new ObjectID(req.params.id);
+  db.collection('Users').findOneAndUpdate({_id: id},
+      {$set: req.body}, {new: true}, (err, result) => {
         if (err) return console.log(err);
-        res.send(result.insertedId);
-    });
+        res.send(result);
+      });
 });
 
 /**
  *
  */
-app.put('/Users/:id', function (req, res) {
-    var id = new ObjectID(req.params.id);
-    db.collection("Users").findOneAndUpdate({_id: id},
-        {$set: req.body}, {new: true}, (err, result) => {
-            if (err) return console.log(err);
-            res.send(result)
-        });
-});
-
-/**
- *
- */
-app.put('/Events/:id', function (req, res) {
-    var id = new ObjectID(req.params.id);
-    db.collection("Events").findOneAndUpdate({_id: id},
-        {$set: req.body}, {new: true}, (err, result) => {
-            if (err) return console.log(err);
-            res.send(result)
-        });
+app.put('/Events/:id', function(req, res) {
+  const id = new ObjectID(req.params.id);
+  db.collection('Events').findOneAndUpdate({_id: id},
+      {$set: req.body}, {new: true}, (err, result) => {
+        if (err) return console.log(err);
+        res.send(result);
+      });
 });
 
 /**
@@ -109,25 +108,25 @@ app.put('/Events/:id', function (req, res) {
  * @param res
  * @param next
  */
-var match_users2events = function (req, res, next) {
-    var interests = req.body.Interests;
-    var latitdec_upper = req.body.latdec + coord_var;
-    var latitdec_lower = req.body.latdec - coord_var;
-    var longitdec_upper = req.body.longdec + coord_var;
-    var longitdec_lower = req.body.longdec - coord_var;
-    if (interests.length >= 1 || interests !== undefined) {
-        db.collection("Users").find({
-            Interests: {$in: interests},
-            Active: true,
-            longdec: {$gte: (longitdec_lower), $lte: (longitdec_upper)},
-            latdec: {$gte: (latitdec_lower), $lte: (latitdec_upper)}
-        }).toArray((err, result) => {
-            if (err) return console.log(err);
-            console.log(result);
-            //TODO Do stuff with the array to find the best matches
-        })
-    }
-    next();
+const match_users2events = function(req, res, next) {
+  const interests = req.body.Interests;
+  const latitdec_upper = req.body.latdec + coord_var;
+  const latitdec_lower = req.body.latdec - coord_var;
+  const longitdec_upper = req.body.longdec + coord_var;
+  const longitdec_lower = req.body.longdec - coord_var;
+  if (interests.length >= 1 || interests !== undefined) {
+    db.collection('Users').find({
+      Interests: {$in: interests},
+      Active: true,
+      longdec: {$gte: (longitdec_lower), $lte: (longitdec_upper)},
+      latdec: {$gte: (latitdec_lower), $lte: (latitdec_upper)},
+    }).toArray((err, result) => {
+      if (err) return console.log(err);
+      console.log(result);
+      // TODO Do stuff with the array to find the best matches
+    });
+  }
+  next();
 };
 
 /*
@@ -137,93 +136,93 @@ Parameters in req: name (name of event), Interests (for event), latdec (lat of e
 /**
  *
  */
-app.post('/Events', [match_users2events], function (req, res, next) {
-    db.collection("Events").insertOne(req.body, (err, result) => {
-        if (err) return console.log(err);
-        // var inserted_id= result.insertedId;
-        res.send(result.insertedId);
-    });
-    msg={
-        "title":"New event",
-        "body":"Please Join"
-    };
-    sendmessage('fev7MsN1fhY:APA91bG0j-lWW-3a1qkUqItdCxP0qghyrt4do8_2sqjpjf4vqq1aep1V2QWTfa3C7OAyxRejm0J98N46hkRbf72SNDbwBxmbyZXK9aQdyHTRZAQJ-o5tPJeeJfjRxiytIaEHiJzFqWaS', msg)
+app.post('/Events', [match_users2events], function(req, res, next) {
+  db.collection('Events').insertOne(req.body, (err, result) => {
+    if (err) return console.log(err);
+    // var inserted_id= result.insertedId;
+    res.send(result.insertedId);
+  });
+  msg={
+    'title': 'New event',
+    'body': 'Please Join',
+  };
+  sendmessage('fev7MsN1fhY:APA91bG0j-lWW-3a1qkUqItdCxP0qghyrt4do8_2sqjpjf4vqq1aep1V2QWTfa3C7OAyxRejm0J98N46hkRbf72SNDbwBxmbyZXK9aQdyHTRZAQJ-o5tPJeeJfjRxiytIaEHiJzFqWaS', msg);
 });
 
 /**
  *
  */
 app.get('/Users', (req, res) => {
-    db.collection("Users").find().toArray((err, result) => {
-        res.send(result);
-    })
+  db.collection('Users').find().toArray((err, result) => {
+    res.send(result);
+  });
 });
 
 /**
  *
  */
 app.get('/Events', (req, res) => {
-    db.collection("Events").find().toArray((err, result) => {
-        res.send(result);
-    })
+  db.collection('Events').find().toArray((err, result) => {
+    res.send(result);
+  });
 });
 
 /**
  *
  */
 app.get('/Users/:id', (req, res) => {
-    console.log("someone retrieved a user");
-    var id = new ObjectID(req.params.id);//req.params.id
-    db.collection("Users").find({'_id': id}).toArray((err, result) => {
-        res.send(result);
-    })
+  console.log('someone retrieved a user');
+  const id = new ObjectID(req.params.id);// req.params.id
+  db.collection('Users').find({'_id': id}).toArray((err, result) => {
+    res.send(result);
+  });
 });
 
 /**
  *
  */
 app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.json({
-        error: {
-            message: err.message
-        }
-    });
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
 });
 
 /**
  *
  */
-app.post('/', function (req, res) {
-    res.end();
+app.post('/', function(req, res) {
+  res.end();
 });
 
-//TODO: implement updating function/call (to update songe parameter of document/json)
+// TODO: implement updating function/call (to update songe parameter of document/json)
 
-//firebase cloud messaging stuff
+// firebase cloud messaging stuff
 /**
  *
  * @param registrationToken
  * @param payload
  */
 function sendmessage(registrationToken, payload) {
-    var message = {notification: payload, token: registrationToken};
-    admin.messaging().send(message)
-        .then((response) => {
-            // Response is a message ID string.
-            console.log('Successfully sent message:', response);
-        })
-        .catch((error) => {
-            console.log('Error sending message:', error);
-        });
+  const message = {notification: payload, token: registrationToken};
+  admin.messaging().send(message)
+      .then((response) => {
+        // Response is a message ID string.
+        console.log('Successfully sent message:', response);
+      })
+      .catch((error) => {
+        console.log('Error sending message:', error);
+      });
 }
 
 /**
  * Initiate REST endpoints on specified port
  * @param port integer which specifies which port the REST endpoints are accessible at
  */
-var server = app.listen(port, function () {
-    //var host = server.address().address
-    var port = server.address().port;
-    console.log("App listening at %s!", port)
+var server = app.listen(port, function() {
+  // var host = server.address().address
+  const port = server.address().port;
+  console.log('App listening at %s!', port);
 });
