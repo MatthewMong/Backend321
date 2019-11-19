ceaf3be38c2d65ee55de154563881d4daf7042f6
Kswizzle <konswic@gmail.com>
2019-11-18 19:06:50 -0800

    This is Pooch, dont update to this

--- tests/Server.test.js
+++ tests/Server.test.js
@@ -81,28 +81,98 @@ describe('GET invalid Event', () => {
     let db;
 
     beforeAll(async () => {
-        __MONGO_URI__=uri;
-        connection = await MongoClient.connect(global.__MONGO_URI__, {
+        //__MONGO_URI__=uri;
+        connection = await MongoClient.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
-        __MONGO_DB_NAME__ = dbName;
-        db = await connection.db(global.__MONGO_DB_NAME__);
+        //__MONGO_DB_NAME__ = dbName;
+        db = await connection.db(dbName);
     });
 
+
     afterAll(async () => {
+        //await db.close();
         await connection.close();
         await db.close();
     });
-
     it('should return a response with HTTP code 200 and an empty array', async () => {
-        // const users = db.collection("Users");
-        // const mockUser = {_id: "5dd1f920fcd2064ba8787a9d", name: 'John'};
+        //const users = db.collection("Users");
+        const mockUser = {_id: "5dbf442a5a3fb30420fc0ebc", name: 'John'};
 
-        const res = await request.get('/Users');
-        expect(res.statusCode).toEqual(200);
+        const res = await request.get("/Users/"+mockUser._id);
+       // expect(res.statusCode).toEqual(200);
         expect(res.body).toEqual("5da61616f3d9a936c43a5bbd");
 
     });
 
+});
+// describe('GET invalid Event', () => {
+//     let connection;
+//     let db;
+//
+//     beforeAll(async () => {
+//         //__MONGO_URI__=uri;
+//         connection = await MongoClient.connect(__MONGO_URI__, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         });
+//        // __MONGO_DB_NAME__ = dbName;
+//         db = await connection.db(__MONGO_DB_NAME__);
+//     });
+//
+//
+//     afterAll(async () => {
+//         //await db.close();
+//         await connection.close();
+//         await db.close();
+//     });
+//     it('should return a response with HTTP code 200 and an empty array', async () => {
+//         const users = db.collection("Users");
+//         const mockUser = {_id: "5dbf442a5a3fb30420fc0ebc", name: 'John'};
+//
+//         await users.insertOne(mockUser);
+//
+//         const insertedUser = await users.findOne({_id: 'some-user-id'});
+//         expect(insertedUser).toEqual(mockUser);
+//
+//         // const res = await request.get("/Users/"+mockUser._id);
+//         // // expect(res.statusCode).toEqual(200);
+//         // expect(res.body).toEqual("5da61616f3d9a936c43a5bbd");
+//
+//     });
+//
+// });
+//
+//
+// const {MongoClient} = require('mongodb');
+
+// const {MongoClient} = require('mongodb');
+
+
+describe('insert', () => {
+    let connection;
+    let db;
+
+    beforeAll(async () => {
+        connection = await MongoClient.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        db = await connection.db();
+    });
+
+    afterAll(async () => {
+        await connection.close();
+    });
+
+    it('should insert a doc into collection', async () => {
+        const users = db.collection('Users');
+
+        const mockUser = {_id: 'some-user-id', name: 'John'};
+        await users.insertOne(mockUser);
+
+        const insertedUser = await request.get("\Users"+mockUser._id);
+        expect(insertedUser.body).toEqual(1);
+    });
 });
